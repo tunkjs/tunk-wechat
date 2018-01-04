d7b0ca509b3442a378e70ab6fac1ffb75b233bf6
steven03.chen <steven03.chen@vipshop.com>
2018-01-04 20:14:21 +0800

    dev

--- tunk-wechat.js
+++ tunk-wechat.js
@@ -2,8 +2,8 @@ var apply = require('apply.js');
 
 (function () {
 
-    var connections = [];
-    var _hidden = true;
+    var connections = [],
+        _hidden = true;
 
     function wechat(utils) {
         var tunk = this;
@@ -17,10 +17,6 @@ var apply = require('apply.js');
             Page(connect(obj, this, 'page'));
         };
 
-        // wechat.Component = function (obj) {
-        //     Component(connect(obj, this, 'component'));
-        // };
-
         utils.hook('setState', function (origin) {
             return function (newState, options) {
                 var pipes = connections[options.moduleName],
@@ -30,50 +26,49 @@ var apply = require('apply.js');
                 origin(newState, options);
 
                 setTimeout(function () {
+                    var stateChangeTargets = [], res;
                     if (pipes && pipes.length) for (var i = 0, l = pipes.length; i < l; i++) if (pipes[i]) {
                         statePath = pipes[i].statePath;
                         // 只更新 changedFields 字段
                         if (statePath[1] && changedFields.indexOf(statePath[1]) === -1) continue;
                         //减少克隆次数，分发出去到达 View 的数据用同一个副本，减少调用 hooks.getState
                         (function (targetObject, propName, newValue, options) {
-                            if(targetObject._hidden_) return;
-                            var tmp = {};
-                            tmp[propName] = newValue;
-                            targetObject.setData(tmp);
-                        })(pipes[i].comp, pipes[i].propName, utils.hooks.getState(statePath, options), options);
+                            if (targetObject._hidden_) {
+                                targetObject._stateDataChanged_ = targetObject._stateDataChanged_ || [];
+                                if(targetObject._stateDataChanged_.indexOf(propName) === -1) targetObject._stateDataChanged_.push(propName);
+                            } else {
+                                if (stateChangeTargets.indexOf(targetObject) === -1) stateChangeTargets.push(targetObject);
+                                targetObject._state_[propName] = newValue;
+                            }
+                        })(pipes[i].comp, pipes[i].propName, !pipes[i].comp._hidden_ && utils.hooks.getState(statePath, options), options);
+                    }
+                    for (var i = 0, l = stateChangeTargets.length; i < l; i++) if (!stateChangeTargets[i]._hidden_) {
+                        _setState(stateChangeTargets[i]);
                     }
                 });
             }
         });
 
-        /**
-         * 
-         * @param {*} obj: Object 
-         * @param {*} context 
-         * @param {*} type: String = ['app', 'page', 'component']
-         */
         function connect(obj, context, type) {
 
             var onLoad = obj.onLoad;
             var onUnload = obj.onUnload
             var onShow = obj.onShow
             var onHide = obj.onHide
-            var attached = obj.attached
-            var detached = obj.detached
 
             var data = obj.data = obj.data || {};
             var state = obj.state;
             var actions = obj.actions;
 
-            if(actions && typeof actions === 'object'){
-                for(var x in actions){
-                    if(actions[x] && typeof actions[x] === 'string' && actions[x].indexOf('.') > -1) {
-                        (function(action){
+            if (actions && typeof actions === 'object') {
+                for (var x in actions) {
+                    if (actions[x] && typeof actions[x] === 'string' && actions[x].indexOf('.') > -1) {
+                        (function (action) {
                             obj[x] = function () {
                                 utils.dispatchAction(action[0], action[1], arguments)
                             }
                         })(actions[x].split('.'));
-                    }else {
+                    } else {
                         throw '[tunk-wechat]:the action value should be like "moduleName.actionName"';
                     }
                 }
@@ -85,23 +80,30 @@ var apply = require('apply.js');
                 return utils.dispatchAction(actionPath[0], actionPath[1], Array.prototype.slice.call(arguments, 1));
             }
 
-            if(type === 'page'){
+            if (type === 'page') {
+
+                // 实时状态容器
+                obj._state_ = {};
+
+                if (state && typeof state === 'object') {
+                    for (var x in state) if (state.hasOwnProperty(x)) {
+                        state[x] = state[x].split('.');
+                    }
+                }
 
                 _defineStateData(state, data);
 
-                obj.onShow = function() {
-                    console.log('obj.onShow', this.data.cd);
+                obj.onShow = function () {
                     this._hidden_ = false;
-                    _refreshState(state, this);
-                    console.log('obj.onShow', this.data.cd);
+                    _refreshState(this);
                     if (onShow) apply(onShow, arguments, this);
                 };
 
-                obj.onHide = function() {
+                obj.onHide = function () {
                     this._hidden_ = true;
                     if (onHide) apply(onHide, arguments, this);
                 };
-                
+
                 obj.onLoad = function () {
                     _initState(state, this);
                     if (onLoad) apply(onLoad, arguments, this);
@@ -119,48 +121,32 @@ var apply = require('apply.js');
                     return apply(onLaunch, arguments, this);
                 };
 
-                obj.onShow = function(){
+                obj.onShow = function () {
                     _hidden = false;
                     if (onShow) apply(onShow, arguments, this);
                 };
 
-                obj.onHide = function(){
+                obj.onHide = function () {
                     _hidden = true;
                     if (onHide) apply(onHide, arguments, this);
-                };
-
-            } else if(type === 'component') {
-
-                _defineStateData(state, data);
-
-                obj.attached = function () {
-                    _initState(state, this);
-                    if (onLoad) apply(onLoad, arguments, this);
-                    console.log('obj.attached', this);
-                    
-                };
-
-                obj.detached = function () {
-                    _disconnect(this);
-                    if (onUnload) apply(onUnload, arguments, this);
                 };
             }
 
             return obj;
         }
 
-        function _defineStateData(state, data){
+        function _defineStateData(state, data) {
             if (state && typeof state === 'object') {
                 for (var x in state) if (state.hasOwnProperty(x)) {
                     data[x] = null;
                 }
             }
         }
-        function _initState(state, context){
-            var tmp = {}; 
+        function _initState(state, context) {
+            var tmp = {};
             if (state && typeof state === 'object') {
                 for (var x in state) if (state.hasOwnProperty(x)) {
-                    tmp[x] = _connectState(context, x, state[x].split('.'));
+                    tmp[x] = _connectState(context, x, state[x]);
                 }
             }
             context.setData(tmp);
@@ -179,19 +165,32 @@ var apply = require('apply.js');
             return utils.hooks.getState(statePath, utils.modules[statePath[0]].options);
         }
 
-        function _refreshState(state, context){
-            var tmp = {}; 
-            var statePath;
-            if (state && typeof state === 'object') {
-                for (var x in state) if (state.hasOwnProperty(x)) {
-                    statePath = state[x].split('.');
-                    tmp[x] = utils.hooks.getState(statePath, utils.modules[statePath[0]].options);
-                }
+        function _setState(target) {
+            var tmp = {};
+            if (target.onBeforeSetState) {
+                res = target.onBeforeStateChange(target._state_);
+                if (res && typeof res !== 'object') throw '[tunk-wechat]:wrong data type from onBeforeSetState.';
+                target.setData(res || target._state_);
+            } else {
+                target.setData(target._state_);
             }
-            context.setData(tmp);
+            target._state_ = {};
+        }
+        function _refreshState(target) {
+            var props = target._stateDataChanged_, statePath;
+            if (props) {
+                for (var i = 0, l = props.length; i < l; i++) {
+                    statePath = target.state[props[i]];
+                    target._state_[props[i]] = utils.hooks.getState(statePath, utils.modules[statePath[0]].options);
+                }
+                _setState(target);
+                
+                target._stateDataChanged_ = null;
+            }
+
         }
 
-        function _disconnect(context){
+        function _disconnect(context) {
             if (context._tunkOptions_) {
                 var stateOption = context._tunkOptions_;
                 var tmp;
